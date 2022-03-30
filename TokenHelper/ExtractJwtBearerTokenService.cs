251f77d6d6cd2cfe99e12b344e4540893869f489
Przemyslaw Seneczko <contact@pjsen.eu>
2022-03-30 09:30:48 +0200

    Initial commit

---
+++ TokenHelper/ExtractJwtBearerTokenService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace TokenHelper
+{
+    public sealed class ExtractJwtBearerTokenService
+    {
+        public string Input { get; set; }
+        public string Output { get; set; }
+
+        private static readonly Regex tokenRegex = new Regex(@".*Bearer ([a-zA-Z0-9_\.\-_]+)""", RegexOptions.Compiled);
+
+        public void Extract()
+        {
+            var tokenMatch = tokenRegex.Match(Input);
+            if (tokenMatch.Success && tokenMatch.Groups.Count > 1)
+            {
+                Output = tokenMatch.Groups[1].Value;
+            }
+            else
+            {
+                throw new TokenExtractionException();
+            }
+        }
+    }
+
+    public class TokenExtractionException : Exception
+    {
+        public TokenExtractionException(string message) : base(message)
+        {
+        }
+
+        public TokenExtractionException()
+        {
+        }
+    }
+}
---
+++ TokenHelper/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace TokenHelper
+{
+    public partial class Form1 : Form
+    {
+        private ExtractJwtBearerTokenService _tokenService;
+
+        public Form1()
+        {
+            InitializeComponent();
+            _tokenService = new ExtractJwtBearerTokenService();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ExtractTokenStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var idat = Clipboard.GetDataObject();
+            var text = idat.GetData(DataFormats.Text) as string;
+
+            _tokenService.Input = text;
+
+            try
+            {
+                _tokenService.Extract();
+                Clipboard.SetText(_tokenService.Output);
+            }
+            catch (TokenExtractionException ex)
+            {
+                DisplayErrorMessage("The text in the clipboard does not contain any recognizable token");
+            }
+
+            //-------------------------------------------------------------------------
+
+            void DisplayErrorMessage(string message)
+            {
+                MessageBox.Show(
+                   message,
+                   "Extraction failed",
+                   MessageBoxButtons.OK,
+                   MessageBoxIcon.Exclamation,
+                   MessageBoxDefaultButton.Button1
+                );
+            }
+        }
+    }
+}
